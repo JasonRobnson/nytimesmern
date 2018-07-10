3d9aadcb4c506b083f745285ad13798c6abef0ab
unknown <bubbastinx@gmail.com>
2018-07-09 20:34:05 -0600

    added to seed.js db is work

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const models = require('./models');
 
 const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/myapp';
+
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,7 +19,16 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nyt-articles');
+
+mongoose.connect(MONGODB_URI)
+mongoose.Promise = global.Promise;
+let db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// mongoose.connect( MONGODB_URI);
+
+
+
 
 app.listen(PORT, () => {
   console.log(`App listening on http://localhost:${PORT}`);
